4d246ca31d54cdf686935cbe9ed390739f0b8a51
pashwiniis <pashwini684@gmail.com>
2021-12-21 16:11:31 +0530

    Check the test user name valid or not

---
+++ UserRegestartionRegex/RegestrationCustomException.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UserRegestartionRegex
+{
+    public class RegistrationCustomExceptions : Exception
+    {
+        public enum ExceptionType
+        {
+            NOT_NULL,
+            INVALID_FIRSTNAME,
+            INVALID_LASTNAME,
+            INVALID_EMAIL,
+            INVALID_MOBILENUMBER,
+            INVALID_PASSWORD
+        }
+        private readonly ExceptionType type;
+        public RegistrationCustomExceptions(ExceptionType Type, String message) : base(message)
+        {
+            this.type = Type;
+        }
+    }
+}
---
+++ UserRegestartionRegex/RegestrationRegexPattern.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace UserRegestartionRegex
+{
+    public class RegestrationRegexPattern
+    {
+        string msg = "";
+        public string regex_firstName = "^[A-Z]{1}[a-z]{2,}$";
+        public string ValidateFirstName(string firstName)
+        {
+            try
+            {
+                if (firstName.Equals(""))
+                {
+                    throw new RegistrationCustomExceptions(RegistrationCustomExceptions.ExceptionType.NOT_NULL, "FirstName Should not be null");
+                }
+                if (Regex.IsMatch(firstName, regex_firstName).Equals(true))
+                {
+                    msg = "validation successfull";
+                }
+                else
+                {
+                    throw new RegistrationCustomExceptions(RegistrationCustomExceptions.ExceptionType.INVALID_FIRSTNAME, "FirstName Should Starts with caps and and have minimum three characters"); ;
+                }
+            }
+            catch (RegistrationCustomExceptions ex)
+            {
+                Console.WriteLine("Exception : "+ex.Message);
+            }
+
+            return msg;
+        }
+    }
+}
